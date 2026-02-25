85a03b01aab0368fa9333a18e64b499a574f1844
ashikzaman1797 <ashik.zaman@fiberathome.net>
2026-02-25 15:46:14 +0000

    updated grafana link

--- src/components/ServerHealth.tsx
+++ src/components/ServerHealth.tsx
@@ -4,7 +4,7 @@ const ServerHealth = () => {
   return (
     <Box width="100%" height="800px">
       <iframe
-        src="https://192.168.30.120:3000/d/N4Cl0972342/idrac-dashboard?orgId=1&refresh=1m&from=1768970357098&to=1768973957099"
+        src="https://192.168.30.120:3000/d/N4Cl0972342/idrac-dashboard?orgId=1&refresh=1m"
         width="100%"
         height="100%"
         frameBorder="0"
